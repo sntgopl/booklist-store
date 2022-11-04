bd83681d59e3ee09ae8176565838d0abaf85ca40
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-11-04 14:00:31 -0300

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -5,49 +5,41 @@ import {
 import Book from './Components/Books';
 import AddBook from './Components/Form';
 
-const NavBar = () => {
-  return (
-    <nav className='NavBar'>
-      <h2 className='NavTitle'>Booklist</h2>
-      <Link to="/">Books</Link>
-      <Link to="/categories">Categories</Link>
-    </nav>
-  );
-}
+const NavBar = () => (
+  <nav className="NavBar">
+    <h2 className="NavTitle">Booklist</h2>
+    <Link to="/">Books</Link>
+    <Link to="/categories">Categories</Link>
+  </nav>
+);
 
-const Books = () => {
-  return (
-    <div className='HomePage'>
-      <ul className='Booklist'>
-        <Book />
-        <Book />
-      </ul>
-      <AddBook />
-    </div>
-  )
-}
+const Books = () => (
+  <div className="HomePage">
+    <ul className="Booklist">
+      <Book />
+      <Book />
+    </ul>
+    <AddBook />
+  </div>
+);
 
-const Categories = () => {
-  return (
-    <div>
-      <ul>
-        <h3>Categories</h3>
-        <li>All</li>
-      </ul>
-    </div>
-  )
-}
+const Categories = () => (
+  <div>
+    <ul>
+      <h3>Categories</h3>
+      <li>All</li>
+    </ul>
+  </div>
+);
 
-const App = () => {
-  return (
-    <div className='App'>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </div>
-  )
-}
+const App = () => (
+  <div className="App">
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
+  </div>
+);
 
 export default App;
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
-const AddBook = () => {
-  return (
-    <div>
-      <form>
-        <label>Add new book</label>
-        <input type='text' placeholder='write the name of the book you want to add'></input>
-        <select name='Category'>
-          <option value='--'>--Please choose an option--</option>
-          <option value='terror'>Terror</option>
-          <option value='drama'>Drama</option>
-        </select>
-        <button type='button'>Add Book</button>
-      </form>
-    </div>
-  );
-}
+const AddBook = () => (
+  <div>
+    <form>
+      <h3>Add new book</h3>
+      <input type="text" placeholder="write the name of the book you want to add" />
+      <select name="Category">
+        <option value="--">--Please choose an option--</option>
+        <option value="terror">Terror</option>
+        <option value="drama">Drama</option>
+      </select>
+      <button type="button">Add Book</button>
+    </form>
+  </div>
+);
 
 export default AddBook;
